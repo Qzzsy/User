2068ffb9aa5efc8be1d04669a43c32accd0426e9
ZSY <zhushiye@live.com>
2018-08-27 23:19:58 +0800

    完善TimerIRQ程序

--- Drivers/Src/TimerIRQ.c
+++ Drivers/Src/TimerIRQ.c
@@ -6,8 +6,35 @@ __weak void LED_TickInc(void)
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* tim_baseHandle)
 {
-    if(tim_baseHandle->Instance == TIM10)
+    if (tim_baseHandle->Instance == TIM1)
+    {
+        /* code */
+        return ;
+    }
+    else if (tim_baseHandle->Instance == TIM2)
+    {
+        /* code */
+        return ;
+    }
+    else if (tim_baseHandle->Instance == TIM3)
+    {
+        /* code */
+        return ;
+    }
+    else if (tim_baseHandle->Instance == TIM4)
+    {
+        /* code */
+        return ;
+    }
+    else if (tim_baseHandle->Instance == TIM5)
+    {
+        /* code */
+        return ;
+    }
+    else if (tim_baseHandle->Instance == TIM10)
     {
         LED_TickInc();
+        return ;
     }
+    
 }
