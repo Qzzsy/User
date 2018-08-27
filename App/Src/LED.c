30b567d95a425d880d27ad87a98dd658ed0b9f45
ZSY <zhushiye@live.com>
2018-08-27 23:17:45 +0800

    完善LED的APP程序

--- App/Src/LED.c
+++ App/Src/LED.c
@@ -1,7 +1,32 @@
+/**
+ ******************************************************************************
+ * @file      led.c
+ * @author    ZSY
+ * @version   V1.0.0
+ * @date      2018-07-08
+ * @brief     实现LED闪烁
+ * @History
+ * Date           Author    version    		   Notes
+ * 2018-06-22      ZSY      V1.0.0          first version.
+ * 2018-08-27      ZSY      V1.0.1          添加对LED控制的宏定义，可以单独使用其中的一个LED
+ */
+/* Includes ------------------------------------------------------------------*/
 #include "led.h"
 
+/* 若使用LL库，请定义USER_EFFI为1 */
 #define USER_EFFI           0
 
+/* 定义使用的LED */
+#define USER_LED0
+#define USER_LED1
+
+#ifdef USER_LED0
+/**
+ * @func    LED0_On
+ * @brief   LED0打开
+ * @note    LED0为低电平打开，若使用高电平打开，请修改GPIO_PIN_RESET为GPIO_PIN_SET
+ * @retval  无
+ */
 void LED0_On(void)
 {
 #if USER_EFFI == 1
@@ -11,6 +36,12 @@ void LED0_On(void)
 #endif
 }
 
+/**
+ * @func    LED0_Off
+ * @brief   LED0关闭
+ * @note    LED0为高电平关闭，若使用低电平关闭，请修改GPIO_PIN_SET为GPIO_PIN_RESET
+ * @retval  无
+ */
 void LED0_Off(void)
 {
 #if USER_EFFI == 1
@@ -19,7 +50,15 @@ void LED0_Off(void)
     HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, GPIO_PIN_SET);
 #endif
 }
+#endif
 
+#ifdef USER_LED1
+/**
+ * @func    LED1_On
+ * @brief   LED1打开
+ * @note    LED0为低电平打开，若使用高电平打开，请修改GPIO_PIN_RESET为GPIO_PIN_SET
+ * @retval  无
+ */
 void LED1_On(void)
 {
 #if USER_EFFI == 1
@@ -29,6 +68,12 @@ void LED1_On(void)
 #endif
 }
 
+/**
+ * @func    LED1_Off
+ * @brief   LED1关闭
+ * @note    LED1为高电平关闭，若使用低电平关闭，请修改GPIO_PIN_SET为GPIO_PIN_RESET
+ * @retval  无
+ */
 void LED1_Off(void)
 {
 #if USER_EFFI == 1
@@ -37,7 +82,14 @@ void LED1_Off(void)
     HAL_GPIO_WritePin(LED0_GPIO_Port, LED1_Pin, GPIO_PIN_SET);
 #endif
 }
+#endif
 
+/**
+ * @func    LED_TickInc
+ * @brief   控制LED闪烁，由中断函数调用
+ * @note
+ * @retval  无
+ */
 void LED_TickInc(void)
 {
     static uint16_t LED_Tick = 0;
@@ -49,13 +101,11 @@ void LED_TickInc(void)
         if (Flag == true)
         {
             Flag = false;
-            LED0_On();
             LED1_Off();
         }
         else
         {
             Flag = true;
-            LED0_Off();
             LED1_On();
         }
     }
--- App/Src/UserMain.c
+++ App/Src/UserMain.c
@@ -1,52 +1,38 @@
 #include "STM32F4xx.h"
 #include "led.h"
-#include "bsp_lcd.h"
-#include "bsp_rtp_touch.h"
-#include "bsp_eeprom_24xx.h"
-#include "GUIDr.h"
-#include "lvgl.h"
-#include "lv_tft.h"
-#include "lv_touchpad.h"
-#include "lv_demo.h"
-#include "arm_math.h"
 #include "Bsp_W25QXX.h"
-#include "lv_test_theme.h"
+#include "BootLoader.h"
+#include "usart.h"
+#include "MyString.h"
 
-uint8_t bsp_TestExtSRAM(void);
+uint32_t buf[2048] = {'\0'};
+
+UART_HandleTypeDef * hUart;
+
+void ConsoleOut(uint8_t * Sendbuf, uint32_t Lenght)
+{
+    HAL_UART_Transmit(hUart, Sendbuf, Lenght, 10);
+}
 
 void UserMainFunc(void)
 {
     uint8_t Buf[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-    uint16_t ta, tb, x, y;
-    BspLCD_FuncInit();
-    BspLCD.Init();
-    BspW25QXX_Init();
-    bsp_TestExtSRAM();
-    GuiSetDeviceAPI(BspLCD_PutPixelNoXY, BspLCD.PutPixel, BspLCD.SetDispWin);
-    
-    GuiClrScr(0x0000);
-    
-    if (Bsp_eeInit() == AT24XX_OK)
-    {
-        GuiClrScr(0xf800);
-    }
-    
-    RTP_Adjust(RTP_NEEDNT_ADJ);
-    
-    GuiSetTextColor(BLACK, WHITE);
+    uint8_t status = 0;
 
-    lv_init();
-    LCD_LvglInit();
-    TouchpadInit();
+    hUart = Uart1_GetHandle();
+    SetConsoleDevice(&ConsoleOut);
 
-//    demo_create();
-    lv_theme_t *th = lv_theme_templ_init(21, &lv_font_dejavu_20);
-    lv_test_theme_1(th);
+    HAL_UART_Receive_IT(hUart, (uint8_t *)&status, 1);
+//    SCB->VTOR = APPLICATION_ADDRESS;
+//    Bootloader_JumpToApplication();
+
+    my_printf("×£ÊÀÒ¶%d\n", 12);
     while(true)
     {
-        lv_tick_inc(5);
-		lv_task_handler();
-        HAL_Delay(5);
+        LED0_On();
+        for (uint32_t i = 0; i < 0x7fffff; i++);
+        LED0_Off();
+        for (uint32_t i = 0; i < 0x7fffff; i++);
     }
 }
 
