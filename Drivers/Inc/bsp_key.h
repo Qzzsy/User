a0a449221a654ec4afc19d942eefdaf039a181da
ZSY <zhushiye@live.com>
2018-09-03 22:59:21 +0800

    添加按键的Bsp文件

---
+++ Drivers/Inc/bsp_key.h
@@ -0,0 +1,53 @@
+/**
+ ******************************************************************************
+ * @file      bsp_key.h
+ * @author    ZSY
+ * @version   V1.0.0
+ * @date      2018-08-31
+ * @brief     该文件实现了按键的处理方法，支持按键的长短按，单双吉，按键的值仅存在一个
+ *            周期，不需要人为清理，在系统上使用的时候需要注意扫描时间跟判断时间的冲突
+ * @History
+ * Date           Author    version    		Notes
+ * 2018-08-31       ZSY     V1.0.0      first version.
+ */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __BSP_KEY_H_
+#define __BSP_KEY_H_
+
+#define KEY_USE_LONGPRESS					/*!< 定义使能长按 */
+#define KEY_USE_DOUBLE_CLICK				/*!< 定义使能双击 */
+
+#define KEY_NUM						(2)		/*!< 按键数量 */
+#define KEY_LONGPRESS_TIME			100		/*!< 扫面周期 */
+#define KEY_DOUBLECLICK_TIME		100		/*!< 双击最大间隔时间 */
+
+/* 共用体 */
+typedef union
+{
+	unsigned int v;
+
+	struct
+	{
+		unsigned int OK : 1;
+		unsigned int Back : 1;
+	}Key;
+}KeyEvent_t;
+
+/* 结构体 */
+typedef struct
+{
+	KeyEvent_t pShort;
+#ifdef KEY_USE_LONGPRESS
+	KeyEvent_t pLong;
+#endif
+#ifdef KEY_USE_DOUBLE_CLICK
+	KeyEvent_t DoubleClick;
+#endif
+}Key_t;
+
+Key_t * GetKeyHandle(void);
+void KeyScan(void);
+
+#endif
+
---
+++ Drivers/Src/bsp_key.c
@@ -0,0 +1,176 @@
+/**
+ ******************************************************************************
+ * @file      bsp_key.c
+ * @author    ZSY
+ * @version   V1.0.0
+ * @date      2018-08-31
+ * @brief     该文件实现了按键的处理方法，支持按键的长短按，单双吉，按键的值仅存在一个
+ *            周期，不需要人为清理，在系统上使用的时候需要注意扫描时间跟判断时间的冲突
+ * @History
+ * Date           Author    version    		Notes
+ * 2018-08-31       ZSY     V1.0.0      first version.
+ */
+	
+/* Includes ------------------------------------------------------------------*/
+#include "bsp_key.h"
+#include "stm32f4xx.h"
+
+/* 定义按键错误的类型 */
+#define KEY_ERROR					(-1)
+
+//typedef unsigned char uint8_t;
+//typedef unsigned short uint16_t;
+//typedef unsigned int uint32_t;
+
+//typedef char int8_t;
+//typedef short int16_t;
+//typedef int int32_t;
+
+/* 初始化按键的事件 */
+Key_t KeyEvent = {
+	0,
+#ifdef KEY_USE_LONGPRESS
+	0,
+#endif
+#ifdef KEY_USE_DOUBLE_CLICK
+	0,
+#endif
+};
+
+/**
+ * @func    GetKeyValue
+ * @brief   get the key value, implement the interface according to the hardware.
+ * @retval  none
+ */
+uint32_t GetKeyValue(void)
+{
+	uint32_t Value = UINT32_MAX;
+
+    Value &= 0xfffffff8;
+    Value |= 0xfffffff9 & (~HAL_GPIO_ReadPin(KEY2_GPIO_Port, KEY2_Pin));
+    Value |= HAL_GPIO_ReadPin(KEY3_GPIO_Port, KEY3_Pin) << 1;
+
+	return ~Value;
+}
+
+/**
+ * @func    KeyScan
+ * @brief   scan the key, the function needs to be scanned periodically.
+ * @retval  none
+ */
+void KeyScan(void)
+{
+    __IO uint8_t i = 0;
+	static Key_t KeyCount= {
+		0,
+#ifdef KEY_USE_LONGPRESS
+		0,
+#endif
+#ifdef KEY_USE_DOUBLE_CLICK
+		0,
+#endif
+	};
+
+#ifdef KEY_USE_LONGPRESS
+	static uint16_t KeyLongPressTime[KEY_NUM] = {0};
+#endif
+#ifdef KEY_USE_DOUBLE_CLICK
+	static uint16_t KeyDoubleClickTime[KEY_NUM] = {0};
+#endif
+
+	uint32_t KeyValue;
+
+	KeyValue = GetKeyValue();
+	
+	if (KeyValue == (uint32_t)KEY_ERROR)
+	{
+		return;
+	}
+
+	KeyEvent.pShort.v = KeyValue & (KeyValue ^ KeyCount.pShort.v);
+	KeyCount.pShort.v = KeyValue;
+
+#ifdef KEY_USE_LONGPRESS
+	for (i = 0; i < KEY_NUM; i++)
+	{
+		KeyLongPressTime[i]++;
+		if (KeyEvent.pShort.v & (1 << i))
+		{
+			KeyLongPressTime[i] = 0;
+
+			continue;
+		}
+
+		if (KeyLongPressTime[i] > KEY_LONGPRESS_TIME)
+		{
+			KeyEvent.pLong.v &= ~(1 << i);
+			KeyEvent.pLong.v |= (1 << i) & ((1 << i) ^ KeyCount.pLong.v);
+			KeyCount.pLong.v &= ~(1 << i);
+			KeyCount.pLong.v |= 1 << i;
+		}
+
+		if (!(KeyCount.pShort.v & (1 << i)))
+		{
+			KeyCount.pLong.v &= ~(1 << i);
+
+			KeyLongPressTime[i] = 0;
+		}
+	}
+#endif
+#ifdef KEY_USE_DOUBLE_CLICK
+	for (i = 0; i < KEY_NUM; i++)
+	{
+		KeyDoubleClickTime[i]++;
+
+		if ((KeyEvent.DoubleClick.v & (1 << i)) && (KeyCount.DoubleClick.v & (1 << i)))
+		{
+			KeyEvent.DoubleClick.v &= ~(1 << i);
+			KeyCount.DoubleClick.v &= ~(1 << i);
+		}
+
+		if (KeyEvent.pShort.v & (1 << i))
+		{
+			if (!(KeyCount.DoubleClick.v & (1 << i)))
+			{
+				KeyDoubleClickTime[i] = 0;
+
+				KeyCount.DoubleClick.v &= ~(1 << i);
+				KeyCount.DoubleClick.v |= 1 << i;
+			}
+			else
+			{
+				if (KeyDoubleClickTime[i] < KEY_DOUBLECLICK_TIME)
+				{
+					KeyEvent.DoubleClick.v &= ~(1 << i);
+					KeyEvent.DoubleClick.v |= 1 << i;
+				}
+				else
+				{
+					KeyCount.DoubleClick.v &= ~(1 << i);
+				}
+			}
+			continue;
+		}
+
+		if (KeyDoubleClickTime[i] >= KEY_DOUBLECLICK_TIME && (KeyCount.DoubleClick.v & (1 << i)))
+		{
+			KeyEvent.DoubleClick.v &= ~(1 << i);
+			KeyCount.DoubleClick.v &= ~(1 << i);
+
+			KeyDoubleClickTime[i] = 0;
+		}
+	}
+#endif
+}
+
+/**
+ * @func    GetKeyHandle
+ * @brief   it will return the key handle.
+ * @retval  KeyEvent
+ */
+Key_t * GetKeyHandle(void)
+{
+	return &KeyEvent;
+}
+
+
