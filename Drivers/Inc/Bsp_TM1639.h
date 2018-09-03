6c2a11ac721e4689edc3611869c603fa6295dcbd
ZSY <zhushiye@live.com>
2018-09-03 22:58:41 +0800

    添加TM1639驱动

---
+++ Drivers/Inc/Bsp_TM1639.h
@@ -0,0 +1,37 @@
+#ifndef _BSP_TM1639_H_
+#define _BSP_TM1639_H_
+
+#if defined STM32F1
+#include "stm32f10x.h"
+#elif defined STM32F4
+#include "stm32f4xx.h"
+#endif
+
+#define TM_MODE_DISP_DIG    0x01
+#define TM_MODE_DISP_LED    0x02
+
+#define DIG0                0xC0
+#define DIG1                0xC2
+#define DIG2                0xC4
+#define DIG3                0xC6
+#define DIG4                0xC8
+#define DIG5                0xCA
+#define DIG6                0xCC
+#define DIG7                0xCE
+
+#define LEVEL_OFF           0x80
+#define LEVEL_1             0x88
+#define LEVEL_2             0x89
+#define LEVEL_4             0x8A
+#define LEVEL_10            0x8B
+#define LEVEL_11            0x8C
+#define LEVEL_12            0x8D
+#define LEVEL_13            0x8E
+#define LEVEL_14            0x8F
+
+void TM1639_Control(uint8_t Cmd);
+void TM1639_DispOn(void);
+void TM1639_DispOff(void);
+void TM1639_Disp(uint8_t * Data, uint8_t Mode);
+
+#endif
---
+++ Drivers/Src/Bsp_TM1639.c
@@ -0,0 +1,177 @@
+#include "Bsp_TM1639.h"
+
+/* 若使用LL库，请定义USER_EFFI为1 */
+#ifndef USER_EFFI
+#define USER_EFFI 0
+#endif
+
+#define TM1639_CLK_PIN      TM1639_CLK_Pin
+#define TM1639_DIO_PIN      TM1639_DIO_Pin  
+#define TM1639_CS_PIN       TM1639_CS_Pin
+
+#define TM1639_CLK_PORT	    TM1639_CLK_GPIO_Port
+#define TM1639_DIO_PORT	    TM1639_DIO_GPIO_Port
+#define TM1639_CS_PORT	    TM1639_CS_GPIO_Port
+
+#if USER_EFFI == 1
+#define TM1639_CLK_WL       LL_GPIO_ResetOutputPin(TM1639_CLK_PORT, TM1639_CLK_PIN)
+#define TM1639_CLK_WH       LL_GPIO_SetOutputPin(TM1639_CLK_PORT, TM1639_CLK_PIN)
+#define TM1639_DIO_WL       LL_GPIO_ResetOutputPin(TM1639_DIO_PORT, TM1639_DIO_PIN)  
+#define TM1639_DIO_WH       LL_GPIO_SetOutputPin(TM1639_DIO_PORT, TM1639_DIO_PIN)
+#define TM1639_CS_WL        LL_GPIO_ResetOutputPin(TM1639_CS_PORT, TM1639_CS_PIN)
+#define TM1639_CS_WH        LL_GPIO_SetOutputPin(TM1639_CS_PORT, TM1639_CS_PIN)
+
+#define TM1639_DIO_R        LL_GPIO_IsInputPinSet(TM1639_DIO_PORT, TM1639_DIO_PIN)
+#elif USER_EFFI == 0
+#define TM1639_CLK_WL       HAL_GPIO_WritePin(TM1639_CLK_PORT, TM1639_CLK_PIN, GPIO_PIN_RESET)
+#define TM1639_CLK_WH       HAL_GPIO_WritePin(TM1639_CLK_PORT, TM1639_CLK_PIN, GPIO_PIN_SET)
+#define TM1639_DIO_WL       HAL_GPIO_WritePin(TM1639_DIO_PORT, TM1639_DIO_PIN, GPIO_PIN_RESET)  
+#define TM1639_DIO_WH       HAL_GPIO_WritePin(TM1639_DIO_PORT, TM1639_DIO_PIN, GPIO_PIN_SET) 
+#define TM1639_CS_WL        HAL_GPIO_WritePin(TM1639_CS_PORT, TM1639_CS_PIN, GPIO_PIN_RESET)
+#define TM1639_CS_WH        HAL_GPIO_WritePin(TM1639_CS_PORT, TM1639_CS_PIN, GPIO_PIN_SET)
+
+#define TM1639_DIO_R        HAL_GPIO_ReadPin(TM1639_DIO_PORT, TM1639_DIO_PIN)
+#endif
+
+/*!< 采用自动地址加一的方式写显示缓存 */
+#define CMD_AUTO_ADDR       0x40
+
+/*!< 采用固定的模式写缓存 */
+#define CMD_STATIONARY_ADDR 0x44
+
+#define CMD_WRITE_DISP      0X40
+#define CMD_READ_KEY        0X42
+
+/*!< 起始地址 */
+#define CMD_START_ADDR      0xC0
+
+#define CMD_DISP_ON         0x81
+#define CMD_DISP_OFF        0x80
+
+#if defined STM32F1
+/* IO方向设置 */
+#define SET_TM1639_DIO_IN()  {TM1639_DIO_PORT->CRH &= 0XFFFF0FFF; TM1639_DIO_PORT->CRH |= (uint32_t)8 << 12;}
+#define SET_TM1639_DIO_OUT() {TM1639_DIO_PORT->CRH &= 0XFFFF0FFF; TM1639_DIO_PORT->CRH |= (uint32_t)3 << 12;}
+#elif defined STM32F4
+/* IO方向设置 */
+#define SET_TM1639_DIO_IN()  {TM1639_DIO_PORT->MODER &= ~(3 << (9 * 2)); TM1639_DIO_PORT->MODER |= (0 << (9 * 2));}	//PB12??????
+#define SET_TM1639_DIO_OUT() {TM1639_DIO_PORT->MODER &= ~(3 << (9 * 2)); TM1639_DIO_PORT->MODER |= (1 << (9 * 2));} 
+#endif
+
+const uint8_t Dofly[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x7f, 0x6f, 0x77, 0x7c, 0x58, 0x5e, 0x79, 0x71};
+
+void TM1639_SendData(uint8_t Data)
+{
+    uint8_t i;
+
+    SET_TM1639_DIO_OUT();
+
+    for (i = 0; i < 8; i++)
+    {
+        TM1639_CLK_WL;
+
+        if ((Data >> i) & 0x01)
+            TM1639_DIO_WH;
+        else
+            TM1639_DIO_WL;
+        
+        for (int j = 200; j > 0; j--);
+
+        TM1639_CLK_WH;
+        
+        for (int j = 200; j > 0; j--);
+    }
+}
+
+uint8_t TM1639_ReadData(void)
+{
+    uint8_t i, RevData = 0x00;
+
+    SET_TM1639_DIO_IN();
+
+    for (i = 0; i < 8; i++)
+    {
+        TM1639_CLK_WL;
+
+        if (TM1639_DIO_R)
+            RevData |= 1 << i;
+        
+        for (int j = 200; j > 0; j--);
+
+        TM1639_CLK_WH;
+        
+        for (int j = 200; j > 0; j--);
+    }
+}
+
+void TM1639_Start(void)
+{
+    TM1639_CS_WL;
+
+    TM1639_CLK_WH;
+}
+
+void TM1639_Stop(void)
+{
+    TM1639_CS_WH;
+    
+    TM1639_CLK_WH;
+}
+
+void TM1639_Control(uint8_t Cmd)
+{
+    TM1639_Start();
+
+    TM1639_SendData(Cmd);
+
+    TM1639_Stop();
+}
+
+void TM1639_DispOn(void)
+{
+    TM1639_Control(CMD_DISP_ON);
+}
+
+void TM1639_DispOff(void)
+{
+    TM1639_Control(CMD_DISP_OFF);
+}
+
+void TM1639_Disp(uint8_t * Data, uint8_t Mode)
+{
+    uint8_t i;
+
+    TM1639_Control(CMD_AUTO_ADDR);
+
+    TM1639_Start();
+
+    if (Mode == TM_MODE_DISP_DIG)
+    {
+        TM1639_SendData(DIG0);
+
+        for (i = 0; i < 2; i++)
+        {
+            TM1639_SendData(Dofly[Data[i]] & 0x0f);
+
+            TM1639_SendData(((Dofly[Data[i]]) >> 4) & 0x0f);
+        }
+    }
+    else if (Mode == TM_MODE_DISP_LED)
+    {
+        TM1639_SendData(DIG1);
+
+        TM1639_SendData((*Data) & 0x0f);
+        
+        TM1639_SendData(((*Data) >> 4) & 0x0f);
+    }
+
+    TM1639_Stop();
+
+    TM1639_Control(LEVEL_1);
+}
+
+void TM1639_ReadKeyValue(void)
+{
+    TM1639_Control(CMD_READ_KEY);
+}
+
